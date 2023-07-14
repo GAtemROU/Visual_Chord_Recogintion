5e84af0c23167346fbf87f46d2907f3fec221b8d
GAtemROU <timur.mikhalevsky@gmail.com>
2023-07-14 14:17:23 +0200

    first model trained, augmented data added

--- src/cnn/model.py
+++ src/cnn/model.py
@@ -5,25 +5,23 @@ import torch.optim as optim
 class CnnChordClassifier(nn.Module):
     def __init__(self, num_classes):
         super(CnnChordClassifier, self).__init__()
-        self.conv1 = nn.Conv2d(3, 32, kernel_size=3, stride=1)
+        self.conv1 = nn.Conv2d(3, 32, kernel_size=3, stride=1, padding=1)
         self.relu1 = nn.ReLU()
-        self.maxpool1 = nn.MaxPool2d(kernel_size=2, stride=2)
-        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, stride=1)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1)
         self.relu2 = nn.ReLU()
-        self.maxpool2 = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.maxpool = nn.MaxPool2d(kernel_size=2, stride=2)
         self.flatten = nn.Flatten()
-        self.fc1 = nn.Linear(64 * 11 * 11, 64)
+        self.fc1 = nn.Linear(64 * 80 * 60, 64)
         self.relu3 = nn.ReLU()
         self.fc2 = nn.Linear(64, num_classes)
 
     def forward(self, x):
-        skip1 = x.copy()
         x = self.conv1(x)
         x = self.relu1(x)
-        skip2 = self.maxpool1(x) + skip1
+        x = self.maxpool(x)
         x = self.conv2(x)
         x = self.relu2(x)
-        x = self.maxpool2(x) + skip2
+        x = self.maxpool(x)
         x = self.flatten(x)
         x = self.fc1(x)
         x = self.relu3(x)
